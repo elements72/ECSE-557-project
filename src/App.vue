a4ec9d6e6583de9e694d6382dc89224232c84559
elements72 <antonio.lopez.8523@gmail.com>
2023-03-21 12:50:58 -0400

    Created a separate component for csv viewer

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,19 @@
 <template>
   <div>
-    <table v-if="parsed" style="width: 100%;">
-      <thead>
-          <tr>
-              <th v-for="(header, key) in content.meta.fields"
-                  v-bind:key="'header-'+key">
-                  {{ header }}
-              </th>
-          </tr>
-      </thead>
-      <tbody>
-          <tr v-for="(row, rowKey) in content.data"
-              v-bind:key="'row-'+rowKey">
-                  <td v-for="(column, columnKey) in content.meta.fields"
-                      v-bind:key="'row-'+rowKey+'-column-'+columnKey">
-                          <input v-model="content.data[rowKey][column]" disabled/>
-                  </td>
-          </tr>
-      </tbody>
-    </table>
-    <input type="file" accept=".csv" @change="handleFileUpload( $event )"/>
+      <CsvViewer> </CsvViewer>
   </div>
 </template>
 
 <script>
 
-import Papa from 'papaparse';
-  
+import CsvViewer from './components/CsvViewer.vue'
+
 export default {
   name: 'App',
   components: {
+    CsvViewer
   },
   methods: {
-    parseFile(){
-    Papa.parse( this.file, {
-        header: true,
-        skipEmptyLines: true,
-        complete: function( results ){
-            this.content = results;
-            this.parsed = true;
-        }.bind(this)
-      } );
-    },
-    shuffle_select(){
-
-    },
-    handleFileUpload( event ){
-    this.file = event.target.files[0];
-    this.parseFile();
-    },
-  },
-  data() {
-      return {
-          file: '',
-          content: [],
-          parsed: false
-      }
   }
 }
 </script>
